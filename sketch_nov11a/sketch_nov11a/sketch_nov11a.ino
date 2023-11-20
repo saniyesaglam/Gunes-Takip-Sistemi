62521debef4ec3da8f73999cda5683dc82fbf727
saniyesaglam <224212036@ogrenci.amasya.edu.tr>
2023-11-21 01:54:04 +0300

    ilerleme
    servo motor ayar detaylandırma

--- sketch_nov11a/sketch_nov11a/sketch_nov11a.ino
+++ sketch_nov11a/sketch_nov11a/sketch_nov11a.ino
@@ -3,3 +3,11 @@
 
 Servo horizontal;
 int servoh = 180;
+
+int servohLimitHigh = 180;
+int servohLimitLow = 65;
+Servo vertical;
+int servov = 45;
+ 
+int servovLimitHigh = 80;
+int servovLimitLow = 15;
